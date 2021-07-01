4b18f6d783703ea27d2e918c3e85ab2b01474001
erp <erp@mmcserp.mmcargoservices.co.za>
2021-07-02 01:16:50 +0200

    Checklist doctype added

---
+++ mmcs/mmcs_custom_application/doctype/checklist/test_checklist.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Bhavik Patel and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestChecklist(unittest.TestCase):
+	pass
---
+++ mmcs/mmcs_custom_application/doctype/checklist_template/checklist_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Bhavik Patel and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Checklist Template', {
+	// refresh: function(frm) {
+
+	// }
+});
